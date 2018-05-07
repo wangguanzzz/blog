32eb25d8097d819902278ba1c525b9912baf1804
Chris Wang <wangguanzzz@126.com>
2018-05-07 18:35:19 +0800

    update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,18 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end 
+
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
 
   def create
     @article = Article.new(article_params)
