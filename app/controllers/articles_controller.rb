cf569d132b904c1030dff05a70f4da5548fdd28b
chriswang <wangguanzzz@126.com>
2018-05-08 19:46:06 +0800

    update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  
   def index
     @articles = Article.all
   end
@@ -34,6 +35,14 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+   
+    redirect_to articles_path
   end
 
 private
